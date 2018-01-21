e445c8b1fa6cd905973c16e79c3db6f8fc42947a
ALYSHA <ALYSHAVAND@GMAIL.COM>
2018-01-21 01:02:03 -0800

    my bad

--- csv_to_geojson.py
+++ csv_to_geojson.py
@@ -25,39 +25,40 @@ f.close()
 
 print(data[0])
 
-geojsonf = open("C:/Users/Alysha/Documents/455/geojson_50records.geoJson","w", encoding="utf-8")
-geojsonf.write("{\n\t\"features\": [")
+geojsonf = open("C:/Users/Alysha/Documents/455/geojson_50records.geojson","w", encoding="utf-8")
+geojsonf.write("{\n\t\"type\":\"FeatureCollection\",")
+geojsonf.write("\n\t\"features\": [")
 
 for x in range(0,rowcount-1):        
     geojsonf.write("\n\t{")#start feature
-    geojsonf.write("\n\t\t'type': 'Feature',")
+    geojsonf.write("\n\t\t\"type\": \"Feature\",")
 
     #PROPERTIES
-    geojsonf.write("\n\t\t'properties': {")
-    geojsonf.write("\n\t\t\t'title': '")
+    geojsonf.write("\n\t\t\"properties\": {")
+    geojsonf.write("\n\t\t\t\"title\": \"")
     geojsonf.write(data[x][0])
-    geojsonf.write("',\n\t\t\t'description': '")
+    geojsonf.write("\",\n\t\t\t\"description\": \"")
     geojsonf.write(data[x][12])
-    geojsonf.write("',\n\t\t\t'colour': '")
+    geojsonf.write("\",\n\t\t\t\"colour\": \"")
     geojsonf.write(data[x][8])
-    geojsonf.write("',\n\t\t\t'location': '")
+    geojsonf.write("\",\n\t\t\t\"location\": \"")
     geojsonf.write(data[x][17])
-    geojsonf.write("'\n\t\t},")
+    geojsonf.write("\"\n\t\t},")
 
     #GEOMETRY
-    geojsonf.write("\n\t\t'geometry': {")
-    geojsonf.write("\n\t\t\t'coordinates': [")
+    geojsonf.write("\n\t\t\"geometry\": {")
+    geojsonf.write("\n\t\t\t\"coordinates\": [")
     geojsonf.write("\n\t\t\t\t")
     geojsonf.write(data[x][19])
     geojsonf.write(",\n\t\t\t\t")
     geojsonf.write(data[x][18])
     geojsonf.write("\n\t\t\t],")
-    geojsonf.write("\n\t\t\t'type': 'point'")
-    geojsonf.write("'\n\t\t}")
+    geojsonf.write("\n\t\t\t\"type\": \"point")
+    geojsonf.write("\"\n\t\t}")
     
     geojsonf.write("\n\t}")#end feature
     if (x < rowcount-2):
         geojsonf.write(",")
 
-geojsonf.write("\n\t],\n\t'type':'FeatureCollection'\n}")
+geojsonf.write("\n\t],\n\t\n}")
 geojsonf.close()
