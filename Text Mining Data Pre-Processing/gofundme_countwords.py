071990c398b5d73f9c3f2b7490348de01e3c24de
ALYSHA <ALYSHAVAND@GMAIL.COM>
2018-03-30 01:01:48 -0700

    completed freq term extraction per fsa

--- Text Mining Data Pre-Processing/gofundme_countwords.py
+++ Text Mining Data Pre-Processing/gofundme_countwords.py
@@ -1,19 +1,36 @@
 #######################################################
 #
-#   Finding most frequent terms per FSA by Aly van D
-#   Using frequent terms found to extract the most common
-#   terms per FSA
+#   Extracting & geocoding most frequent terms per FSA
+#   by Aly van D
 #
 #######################################################
 
 import csv
 import numpy as np
+import requests as req
 
 fields = []
 postcodenames = []
 data = []
-numberOfRecords = 1808
 dict = {}
+
+###### Geocoding op ######
+def geocodeOp(pc):
+    while True:
+        try:
+            print("Searching")
+            url = "https://maps.googleapis.com/maps/api/geocode/json?address="+pc+" canada"
+            print(url)
+            response = req.get(url)
+            res_json_payload = response.json()
+            latitude = res_json_payload['results'][0]['geometry']['location']['lat']
+            longitude = res_json_payload['results'][0]['geometry']['location']['lng']
+            break;
+        except:
+            print("Retrying")
+    arr = [latitude, longitude]
+    return arr
+
 
 ###### MAIN #####
 
@@ -27,7 +44,8 @@ for row in reader:
     rowcount+=1
 f.close()
  
-f = open('C:/Users/Alysha/Documents/455/textmining/textmining_descriptions.csv')
+#f = open('C:/Users/Alysha/Documents/455/textmining/textmining_descriptions.csv')
+f = open('C:/Users/Alysha/Documents/455/textmining/textmining_titles.csv')
 reader = csv.reader(f)
 
 rowcount = 0
@@ -35,39 +53,35 @@ for row in reader:
     if rowcount == 0:
         fields = row  
     else:
-        #print(row[1])
         data.append(row[1:])
     rowcount+=1
 f.close()
-#print(data[0])
 
-#dict.update({postcodenames[16]: data[0][1:]})
-#print(dict)
+print(fields[2:])
+freqterms = fields[2:]
 
-print(fields[1:])
-
-#for x in range(0,rowcount-1):
 for x in range(0,rowcount-1):
     if dict.has_key(data[x][0]):
         a = np.asarray(dict.get(data[x][0]),dtype=int)
         b = np.asarray(data[x][1:],dtype=int)
         sumResult = np.add(a,b)
         s = sumResult.tolist()        
-        #print(s)
         dict.update({data[x][0]: s})
     else:
         dict.update({data[x][0]: data[x][1:]})
-#print(dict)
 
-# finish this:
-with open('C:/Users/Alysha/Documents/455/textFrequenciesperFSA.csv', 'w') as csvfile:
-    fieldnames = fields[1:]
-    writer = csv.writer(csvfile, fieldnames=fieldnames, lineterminator='\n')
-    writer.writeheader()
+with open('C:/Users/Alysha/Documents/455/textmining/textFrequenciesperFSA.csv', 'w') as csvfile:
+    fieldnames = ["Postcode", "MostFrequentTerm", "Count", "Latitude", "Longitude"]
+    writer = csv.writer(csvfile, lineterminator='\n')
+    writer.writerow(fieldnames)
     for j in dict.items():
-       newStr = str(fieldnames[0]): j[0]
-       for i in len(fieldnames-1):
-           print(i)
-       writer.writerow(j)
+       curr = ""
+       maxCount = 0
+       for k in range(0,len(j[1])):
+            if j[1][k] >= maxCount:
+                maxCount = j[1][k]
+                curr = freqterms[k]
+       latlng = geocodeOp(j[0]);
+       writer.writerow([j[0], curr, maxCount, latlng[0], latlng[1]])
 
 
